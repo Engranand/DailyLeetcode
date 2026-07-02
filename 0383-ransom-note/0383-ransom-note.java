6c492c41a412a026d5a755abda8b02de8e17f979
Anand Raj <anand02ok@gmail.com>
2026-07-03 05:16:59 +0530

    Time: 15 ms (31.36%), Space: 47.1 MB (26.41%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count frequency of each character in magazine
+        for (char ch : magazine.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        // Check characters of ransomNote
+        for (char ch : ransomNote.toCharArray()) {
+            if (!map.containsKey(ch) || map.get(ch) == 0) {
+                return false;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+        }
+
+        return true;
+    }
+}
