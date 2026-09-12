312e8ffcc18acdc7d6268a923ed1e47272db96ab
Anand Raj <anand02ok@gmail.com>
2026-09-12 23:52:56 +0530

    Time: 1 ms (98.09%), Space: 44.6 MB (39.51%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+     LinkedList<List<Integer>> result = new LinkedList<>();
+     //List<List<Integer>> result = new ArrayList<>();
+     // use list when using collection.reverse 
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.addFirst(level);    // 🔥 reverse ki zarurat nahi yaha agar allowerd nhi h revsre toh ye use krr sakte hai 
+
+
+           //result.add(level);
+        }
+
+       // Collections.reverse(result); //yato directly revesre use krlo if allowed 
+
+
+        return result;
+    }
+}
