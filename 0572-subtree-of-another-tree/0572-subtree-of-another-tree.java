1481825b258dc7d9f9245b286f8eb3766fb9a47f
Anand Raj <anand02ok@gmail.com>
2026-09-16 00:32:38 +0530

    Time: 3 ms (64.55%), Space: 46.4 MB (52.52%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+
+        if (subRoot == null) {
+            return true;
+        }
+
+        if (root == null) {
+            return false;
+        }
+
+        if (same(root, subRoot)) {
+            return true;
+        }
+
+        boolean left = isSubtree(root.left, subRoot);
+        boolean right = isSubtree(root.right, subRoot);
+
+        return left || right;
+    }
+
+
+    public boolean same(TreeNode root1, TreeNode root2) {
+
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+
+        if (root1.val != root2.val) {
+            return false;
+        }
+
+        boolean left = same(root1.left, root2.left);
+        boolean right = same(root1.right, root2.right);
+
+        return left && right;
+    }
+}
