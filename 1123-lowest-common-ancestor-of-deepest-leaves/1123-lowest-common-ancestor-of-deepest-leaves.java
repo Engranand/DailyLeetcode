15c21e79944e924e1a84b651185f3cfc09a7c36c
Anand Raj <anand02ok@gmail.com>
2026-09-19 01:07:34 +0530

    Time: 0 ms (100%), Space: 45.4 MB (97.79%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    // 1. Main function
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+
+        int depth = maxDepth(root);
+
+        return findLCA(root, depth);
+    }
+
+    // 2. Find maximum depth
+    public int maxDepth(TreeNode root) {
+
+        if (root == null)
+            return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    // 3. Find LCA at deepest level
+    public TreeNode findLCA(TreeNode root, int depth) {
+
+        if (root == null)
+            return null;
+
+        if (depth == 1)
+            return root;
+
+        TreeNode left = findLCA(root.left, depth - 1);
+        TreeNode right = findLCA(root.right, depth - 1);
+
+        if (left != null && right != null)
+            return root;
+
+        if (left != null)
+            return left;
+
+        return right;
+    }
+}
